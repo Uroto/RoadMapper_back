90c593a8e6cc523932f9a7f281c407c5cab6a758
0fk0 <greeeen_ken41ei39jin@icloud.com>
2023-09-30 12:53:10 +0900

    fix:ポートを環境変数へ

--- src/main.ts
+++ src/main.ts
@@ -4,8 +4,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:3000',
+    origin: process.env.CORS_ORIGIN || "http://localhost:3000",
   });
-  await app.listen(5000);
+  await app.listen(process.env.PORT || 5000);
 }
 bootstrap();
